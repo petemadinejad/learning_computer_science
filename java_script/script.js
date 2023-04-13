a0b10dbcd92b71eed08d3415f37b7ef075d42f00
petemadinejad <parisaetemadinejad.job@gmail.com>
2023-04-13 12:16:54 +0330

    start learn java script

--- java_script/script.js
+++ java_script/script.js
@@ -1,3 +1,12 @@
 // im so happy because i wanna study javascript
 console.log("neon");
  console.log(2+2);
+ let new_var=20;
+ console.log(new_var);
+ let fname="ali";
+ fname="reza";
+ let number;
+ const sex="male";
+ let age=32
+ console.log(age)
+
