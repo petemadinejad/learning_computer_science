972804cd94636e7d2d0011586b7c9ed7c5f358dc
petemadinejad <parisaetemadinejad.job@gmail.com>
2023-05-15 17:11:59 +0330

    learning js

--- java_script/script.js
+++ java_script/script.js
@@ -26,5 +26,67 @@ console.log("neon");
  }
  console.log(typeof person)
  console.log(person["admin"])
+let users=["ali",'mohammad']
+console.log(users)
+console.log(users[2])
+users[2]="zahra"
+console.log(users)
+function Sum(name,age){
+   let calc=2+2
+   console.log(calc)
+   console.log("yessssssssssssssssssssssss")
+   console.log("hello "+name+ "you are "+age+" years old")
+   console.log(age)
+}
+function square(r){
+   return 3.14*r**2
 
+}
+let res=Sum("parisa",30)
+console.log(res)
+console.log(square(5))
+let x=5
+let y=10
+console.log(5+10)
+console.log(x%=2)
+console.log(x>1)
 
+if(10){
+   console.log("ok")
+}
+let role="admin"
+switch(role){
+   case "guest":
+      console.log("user")
+      break
+   case "admin":
+      console.log("admin")
+      break
+   default:
+      console.log("unknown user")
+   
+}
+for(let i=0;i<10;i+=2){
+   console.log("accept")
+   console.log(i)
+}
+// let i=5
+// while(1){
+//    console.log("yes")
+//    i=i-1
+// }
+d=3
+do{
+console.log("yesssss")
+}while(d>5)
+const person2={"name":"parisa","age":28}
+
+for (let key in person2){
+   console.log(key)
+   console.log(person2[key])
+}
+
+const colors=['red','green','blue']
+
+for (let index in colors)
+   console.log(index)
